ef541d5b9a7a0f3d870820d11056a2c1d74f93b6
petrsokerin <petya4343@mail.ru>
2022-03-14 13:49:13 +0300

    adding portfolio calculation and changing pipeline

--- utils/dataloading.py
+++ utils/dataloading.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pandas_datareader.data as web
 from tqdm import tqdm
 
-start_date = '2018-01-01'
+start_date = '2015-01-01'
 end_date = '2022-02-01'
 path_df_sect = 'data/tickers_sectors.csv'
 path_to_save = 'data/ticker_data_{}.csv'
@@ -31,9 +31,9 @@ def generating_datasets(path_df_sect, start_date, end_date, path_to_save, meanin
         df.to_csv(path_to_save.format(meaning))
 
 if __name__ == "__main__":
-	#generating_datasets(path_df_sect, start_date, end_date, path_to_save, meaning_dfs = ['Close', 'Volume'])
-    meaning='Close'
-    tickers = ['^GSPC']
-    df = loading_dataset(tickers, start_date, end_date, meaning)
-    df.to_csv(path_to_save.format('SP500'))
+	generating_datasets(path_df_sect, start_date, end_date, path_to_save, meaning_dfs = ['Close'])
+    # meaning='Close'
+    # tickers = ['^GSPC']
+    # df = loading_dataset(tickers, start_date, end_date, meaning)
+    # df.to_csv(path_to_save.format('SP500'))
 
