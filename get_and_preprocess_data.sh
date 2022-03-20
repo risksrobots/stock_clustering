0e151db87fbae58dcd997a037bfd83fb4af542f7
Andrew <andpuc23@gmail.com>
2022-03-20 19:39:58 +0300

    fix requirements

--- get_and_preprocess_data.sh
+++ get_and_preprocess_data.sh
@@ -1,5 +1,4 @@
 cd utils
-python getting_sectors.py
 python dataloading.py
 python general_preprocessing.py
 python table_feature_calculation.py
--- install_reqs.sh
+++ install_reqs.sh
@@ -1,2 +1,1 @@
-git clone https://github.com/lit26/finvizfinance.git
 pip install -r requirements.txt
